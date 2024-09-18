0966c9e1b3074d1e8643087b5f1dbac99762b45a
LETSO27 <letsomogwera61@gmail.com>
2024-09-18 23:06:27 +0200

    add index.html and index.j

---
+++ index.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+const display = document.querySelector(".display");
+
+function printInput(userInput) {
+  display.innerHTML = "";
+  display.innerHTML = userInput;
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const userData = input.value;
+  printInput(userData);
+});
